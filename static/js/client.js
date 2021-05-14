8e42b22b0517ece8701c481d48b8a7c4fa484011
Diego Alvarez <andresdaap2015@gmail.com>
2021-05-14 04:33:26 -0400

    Support retrieve Calendars &  Events

--- static/js/client.js
+++ static/js/client.js
@@ -53,39 +53,12 @@ const getCode = () => {
   }
 }
 
-const refresToken = () => {
-  const urlRefresh = "https://oauth2.googleapis.com/token";
-  const client_id = "561037342448-7698atk72j4ma0jb7gsbr1vdl698q61r.apps.googleusercontent.com";
-  const client_secret = "CsoDNyz1KpqU5v6hOgQ-BXnc";
-  const refresh_token = localStorage.getItem("refresh_token");
-
-  fetch(urlRefresh, {
-    method: "POST",
-    headers: {
-      "Content-Type": "x-www-urlencoded"
-    },
-    body: `client_id=${client_id}&=client_secret=${client_secret}&refresh_token=${refresh_token}&grant_type=refresh_token`
-  })
-    .then(response => {
-      console.log(response);
-      if (response.ok) {
-        response.json()
-          .then(res => {
-            console.log(res);
-            localStorage.setItem("token_access", res.access_token);
-          })
-          .catch(err => console.log(err));
-      }
-    })
-    .catch(err => console.log(err));
-}
-
 const getCalendarList = () => {
   let formData = new FormData();
   formData.append("token", localStorage.getItem("token_access"));
   formData.append("refresh", localStorage.getItem("refresh_token"));
 
-  fetch("client/client.php", {
+  fetch("client/calendars.php", {
     method: "POST",
     body: formData
   })
@@ -95,6 +68,58 @@ const getCalendarList = () => {
         response.json()
           .then(res => {
             console.log(res);
+            if (res.token != localStorage.getItem("token_access")) {
+              localStorage.setItem("token_access", res.token);
+            }
+            writeCalendars(res.data.calendars, "#calendars");
+          })
+          .catch(err => console.log(err));
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+const writeCalendars = (arrayElements, elementDOM) => {
+  let div = document.querySelector(elementDOM);
+  arrayElements.map(element => {
+    if (element.accessRole === "writer") {
+      div.innerHTML = (`<h4>${element.accessRole} | ${element.id}</h4>`);
+      localStorage.setItem("calendar", element.id);
+    }
+  })
+}
+
+const writeEvents = (arrayElements, elementDOM) => {
+  let div = document.querySelector(elementDOM);
+  arrayElements.map(element => {
+    div.innerHTML = (`<li>${element.status} | ${element.id} | ${element.htmlLink} | ${element.creator.email}</li>`);
+  });
+  if (arrayElements.length === 0) {
+    div.innerHTML = `<a href="event.html">Create Event</a>`;
+  }
+}
+
+const getEvents = () => {
+  let formData = new FormData();
+  formData.append("token", localStorage.getItem("token_access"));
+  formData.append("refresh", localStorage.getItem("refresh_token"));
+  formData.append("calendarId", localStorage.getItem("calendar"));
+  formData.append("reason", "list");
+
+  fetch("client/events.php", {
+    method: "POST",
+    body: formData
+  })
+    .then(response => {
+      console.log(response);
+      if (response.ok) {
+        response.json()
+          .then(res => {
+            console.log(res);
+            if (res.token != localStorage.getItem("token_access")) {
+              localStorage.setItem("token_access", res.token);
+            }
+            writeEvents(res.data.events, "#events");
           })
           .catch(err => console.log(err));
       }
@@ -108,5 +133,9 @@ if (localStorage.getItem("code_auth") !== null) {
   buttonToken.style.display = "block";
   buttonToken.addEventListener("click", getToken);
   document.querySelector("#oauth").style.display = "none";
+}
+
+if (localStorage.getItem("refresh_token") !== null) {
+  buttonToken.style.display = "none";
 }
 
